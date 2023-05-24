cad871fd5d3ed8385c4816e687129f4be54c7ada
Евгений Минеев <ew.mineew@ya.ru>
2023-05-25 00:37:12 +0800

    Добавил модели

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator
 from django.db import models
 
 from ingredients.models import Ingredient
@@ -8,13 +9,87 @@ User = get_user_model()
 
 
 class Recipe(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='recipe_images')
-    description = models.TextField()
-    ingredients = models.ManyToManyField(Ingredient)
-    tags = models.ManyToManyField(Tag)
-    cooking_time = models.IntegerField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes',
+    )
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название рецепта',
+    )
+    image = models.ImageField(upload_to='recipes/recipe_images')
+    text = models.TextField(verbose_name='Описание рецепта')
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through='RecipeIngredient',
+        related_name='recipes',
+        verbose_name='Ингредиенты',
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        through='RecipeTags',
+        related_name='recipes'
+    )
+    cooking_time = models.PositiveSmallIntegerField(
+        validators=(MinValueValidator(1),),
+        verbose_name='Время приготовления в минутах',
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации',
+        auto_now_add=True,
+    )
+
+    class Meta:
+        ordering = ('-pub_date',)
+        verbose_name = 'рецепт'
 
     def __str__(self):
-        return self.title
+        return self.name
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        verbose_name='рецепт'
+    )
+    ingridient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.PROTECT,
+        verbose_name='ингредиент'
+    )
+    amount = models.PositiveSmallIntegerField(
+        validators=(MinValueValidator(1),),
+        verbose_name='количество'
+    )
+
+    class Meta:
+        verbose_name = 'ингредиент в рецепте'
+
+    def __str__(self):
+        return f'{self.recipe}: {self.ingridient} в количестве {self.amount}'
+    
+class RecipeTag(models.Model):
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        verbose_name='рецепт',
+    )
+    tag = models.ForeignKey(
+        Tag,
+        on_delete=models.CASCADE,
+        verbose_name='тег'
+    )
+
+    class Meta:
+        verbose_name = 'тег'
+
+    def __str__(self):
+        return f'Рецепту {self.recipe} присвоен тег {self.tag}'
+    
+class Favorite(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+
+    )
