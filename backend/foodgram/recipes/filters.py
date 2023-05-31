d6a70165e076d669fddd5e2c78cc27bd0b9708f6
Евгений Минеев <ew.mineew@ya.ru>
2023-05-31 11:59:36 +0800

    Дорабатываю

---
+++ backend/foodgram/recipes/filters.py
@@ -0,0 +1,59 @@
+from distutils.util import strtobool
+
+from django_filters import rest_framework
+
+from .models import Favorite, Recipe, ShoppingList
+from tags.models import Tag
+
+OPTIONS = (
+    ('0', 'False'),
+    ('1', 'True')
+)
+
+
+class RecipeFilter(rest_framework.FilterSet):
+    author = rest_framework.NumberFilter(
+        field_name='author',
+        lookup_expr='exact'
+    )
+    tags = rest_framework.ModelMultipleChoiceFilter(
+        field_name='tags__slug',
+        queryset=Tag.objects.all(),
+        to_field_name='slug',
+    )
+    is_favorited = rest_framework.ChoiceFilter(
+        options=OPTIONS,
+        method='is_favorited_method'
+    )
+    is_in_shopping_cart = rest_framework.ChoiceFilter(
+        options=OPTIONS,
+        method='is_in_shopping_cart_method'
+    )
+
+    def is_favoreted_method(self, queryset, name, value):
+        if self.request.user.is_anonymous:
+            return Recipe.objects.none()
+
+        favorites = Favorite.objects.filter(user=self.request.user)
+        recipes = [item.recipe.id for item in favorites]
+        new_queryset = queryset.filter(id__in=recipes)
+
+        if not strtobool(value):
+            return queryset.difference(new_queryset)
+        return queryset.filter(id__in=recipes)
+
+    def is_in_shopping_cart_method(self, queryset, name, value):
+        if self.request.user.is_anonymous:
+            return Recipe.objects.none()
+
+        shopping_cart = ShoppingList.objects.filter(user=self.request.user)
+        recipes = [item.recipe.id for item in shopping_cart]
+        new_queryset = queryset.filter(id__in=recipes)
+
+        if not strtobool(value):
+            return queryset.difference(new_queryset)
+        return queryset.filter(id__in=recipes)
+
+    class Meta:
+        model = Recipe
+        fields = ('tags', 'author')
--- backend/foodgram/recipes/views.py
+++ backend/foodgram/recipes/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets
 
-# Create your views here.
+from .filters import RecipeFilter
+from .models import Recipe
+from .permissions import IsAuthorOrAdminPermission
+from .serializers import CreateUpdateRecipeSerializer, RecipeSerializer
+from users.pagination import CustomPageNumberPagination
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+    filterset_class = RecipeFilter
+    filter_backends = (DjangoFilterBackend,)
+    pagination_class = CustomPageNumberPagination
+    permission_classes = (IsAuthorOrAdminPermission,)
+
+    def get_serializer_class(self):
+        if self.action in ('create', 'partial_update'):
+            return CreateUpdateRecipeSerializer
+        return RecipeSerializer
